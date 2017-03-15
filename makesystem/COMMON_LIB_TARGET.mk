fb87d46a8a6c346a8e87dee280d1fc2dc8b2a5a3
BALAMURUGAN SOUPPOURAYEN <balamurugan.douxure@gmail.com>
2017-03-15 10:58:04 +0530

    Removed makesystem folder.

--- makesystem/COMMON_LIB_TARGET.mk
+++
@@ -1,9 +0,0 @@
-# COMMON_LIB_TARGET.mk
-#
-# Copyright (C) 2017, Makesystem, Balamurugan Souppourayen
-#
-# This software may be modified and distributed under the terms
-# of the MIT license.  See the LICENSE file for details.
-#
-
-include ../../makesystem/common.mk
